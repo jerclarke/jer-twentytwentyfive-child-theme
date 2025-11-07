3ed63f0c221f1b308e53a651dbb5472e87e67d5c
Jer Clarke <jer@simianuprising.com>
2025-11-07 17:56:58 -0600

    ⚙️ add new pattern with jer-intro and show it by default on home

---
+++ patterns/jer-intro-media-and-text.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Title: Jer's intro with media and text, backed up just in case
+ * Slug: jer-twentytwentyfive-child-theme/jer-intro-media-and-text
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+
+<!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:media-text {"align":"wide","mediaId":8881,"mediaLink":"https://simianup.test/jer-karma-avatar-square/","mediaType":"image","mediaWidth":33,"imageFill":false,"className":"round-image-container","style":{"border":{"radius":"0px","width":"0px","style":"none"}}} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile round-image-container" style="border-style:none;border-width:0px;border-radius:0px;grid-template-columns:33% auto"><figure class="wp-block-media-text__media"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/jer-karma-avatar-square-800x800.png" alt="" class="wp-image-8881 size-full"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.2"}},"fontSize":"x-large"} -->
+<p class="has-x-large-font-size" style="line-height:1.2">Jer Clarke is a web developer based in Mexico City. Jer builds <a href="https://globalvoices.org/"><strong>Global&nbsp;Voices</strong></a> with <strong><a href="http://wordpress.org/">WordPress</a></strong>&nbsp;and loves everything.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Jer believes in healing from addiction with Buddhist practice and&nbsp;<strong><a href="http://recoverydharma.org/">Recovery Dharma</a></strong>.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"left"} -->
+<p class="has-text-align-left">Jer’s pronouns are&nbsp;<a href="http://simianuprising.com/2017/08/05/about-jer-and-gender/"><strong>They/Them</strong></a></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
