834aeb15e18dfee3c292db898b6a0cdc8f6f8c55
Jer Clarke <jer@simianuprising.com>
2025-11-10 12:25:47 -0600

    ⚙️ override and customize 404.html to match the pages

---
+++ patterns/jer-404.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Title: 404
+ * Slug: jer-twentytwentyfive-child-theme/jer-404
+ * Inserter: no
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|50","right":"0","left":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--50);padding-right:0;padding-left:0">
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50","left":"var:preset|spacing|50"}}}} -->
+	<div class="wp-block-columns alignwide">
+		<!-- wp:column {"verticalAlignment":"bottom"} -->
+		<div class="wp-block-column is-vertically-aligned-bottom">
+			<!-- wp:group {"layout":{"type":"default"}} -->
+			<div class="wp-block-group">
+				<!-- wp:heading {"level":1} -->
+				<h1 class="wp-block-heading">
+					<?php echo esc_html_x( '404 Page not found', '404 error message', 'twentytwentyfive' ); ?>
+				</h1>
+				<!-- /wp:heading -->
+				<!-- wp:group {"backgroundColor":"accent-3","spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}},"layout":{"type":"default"}} -->
+			<div class="wp-block-group has-accent-3-background-color" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30)">
+					<!-- wp:paragraph -->
+					<p><?php echo esc_html_x( 'The page you are looking for doesn\'t exist, or it has been moved. Please try searching using the form below.', '404 error message', 'twentytwentyfive' ); ?></p>
+					<!-- /wp:paragraph -->
+					<!-- wp:pattern {"slug":"twentytwentyfive/hidden-search"} /-->
+				</div>
+				<!-- /wp:group -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
