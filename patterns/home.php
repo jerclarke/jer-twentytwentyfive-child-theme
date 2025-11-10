d5635d32e18a26ddc2d6387dda38ffa2cbdc6aac
Jer Clarke <jer@simianuprising.com>
2025-11-10 15:49:16 -0600

    ⚙️ move home.html template contents into home.html template part and integrate new jer-widget-grid pattern

---
+++ patterns/home.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Jer's default home template with intro and widget grid
+ * Slug: jer-twentytwentyfive-child-theme/home
+ * Inserter: no
+ */
+?>
+
+
+<!-- wp:template-part {"slug":"header"} /-->
+
+<!-- wp:template-part {"slug":"jer-intro"} /-->
+
+<!-- wp:template-part {"slug":"jer-widget-grid"} /-->
+
+<!-- wp:group {"tagName":"main","style":{"spacing":{"margin":{"top":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
+<main class="wp-block-group" style="margin-top:var(--wp--preset--spacing--60)">
+	<!-- wp:heading {"textAlign":"left","level":1,"layout":{"type":"default"}} -->
+	<h1 class="wp-block-heading wp-block-query-title has-text-align-left">Recent blog posts</h1>
+	<!-- /wp:heading -->
+	<!-- wp:pattern {"slug":"twentytwentyfive/template-query-loop"} /-->
+</main>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer"} /-->
