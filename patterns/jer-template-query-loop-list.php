e271b0a21f3aa9ca7d7828852d841a36f66f409b
Jer Clarke <jer@simianuprising.com>
2025-11-11 10:01:03 -0600

    ⚙️ update home to use newly updated list query loop in a centered contentSize column

--- patterns/jer-template-query-loop-list.php
+++ patterns/jer-template-query-loop-list.php
@@ -12,21 +12,32 @@
  */
 
 ?>
-<!-- wp:query {"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"taxQuery":null,"parents":[]},"align":"wide","layout":{"type":"default"}} -->
-<div class="wp-block-query alignwide">
-		<!-- wp:post-template {"fontSize":"tiny","layout":{"type":"grid","columnCount":3,"minimumColumnWidth":null}} -->
-			<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"backgroundColor":"accent-3","layout":{"type":"constrained","justifyContent":"center"}} -->
-			<div class="wp-block-group has-accent-3-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30)">
-			<!-- wp:post-featured-image {"isLink":true,"aspectRatio":"16/9","style":{"spacing":{"margin":{"top":"0px","bottom":"var:preset|spacing|30"}}}} /-->
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+<!-- wp:query {"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"taxQuery":null,"parents":[]}} -->
+	<div class="wp-block-query">
+		<!-- wp:post-template -->
+		<!-- wp:columns {"verticalAlignment":"center"} -->
+		<div class="wp-block-columns are-vertically-aligned-center">
+			<!-- wp:column {"verticalAlignment":"center","width":"33.3%"} -->
+			<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:33.3%">
+				<!-- wp:post-featured-image {"isLink":true, "aspectRatio":"16/9"} /-->
+			</div>
+			<!-- /wp:column -->
 
-			<!-- wp:post-title {"isLink":true,"fontSize":"large"} /-->
-			<!-- wp:post-terms {"term":"category","fontSize":"small"} /-->
- 			<!-- wp:post-date {"isLink":true,"fontSize":"small"} /-->
-			<?php
-			//<!-- wp:post-excerpt {"align":"full","fontSize":"medium"} /-->
-			?>
+			<!-- wp:column {"verticalAlignment":"center","width":"66.6%"} -->
+				<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.6%">
+				<!-- wp:post-title {"isLink":true,"fontSize":"x-large"} /-->
+
+				<!-- wp:post-terms {"term":"category","fontSize":"small"} /-->
+
+				<!-- wp:post-date {"isLink":true,"fontSize":"small"} /-->
+			</div>
+			<!-- /wp:column -->
+			</div>
+		<!-- /wp:columns -->
+		<!-- /wp:post-template -->
 		</div>
-		<!-- /wp:group -->
 	<!-- /wp:post-template -->
 	 
 	<!-- wp:query-no-results -->
@@ -39,8 +50,8 @@
 		<!-- /wp:group -->
 	<!-- /wp:query-no-results -->
 	 
-	<!-- wp:group {"align":"wide","layout":{"type":"constrained"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}}} -->
-	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40);">
+	<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}}} -->
+	<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);">
 		<!-- wp:query-pagination {"paginationArrow":"arrow","align":"wide","layout":{"type":"flex","justifyContent":"center"}} -->
 			<!-- wp:query-pagination-previous /-->
 			<!-- wp:query-pagination-numbers /-->
@@ -50,3 +61,5 @@
 	<!-- /wp:group -->
 </div>
 <!-- /wp:query -->
+</div>
+<!-- /wp:group -->
