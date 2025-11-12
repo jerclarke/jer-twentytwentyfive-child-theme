991aacf5e7df72ab563d42e1e9f77b261b0f76cc
Jer Clarke <jer@simianuprising.com>
2025-11-12 10:03:00 -0600

    ⚙️ update home to hide intro widgets when is_paged

--- patterns/home.php
+++ patterns/home.php
@@ -4,19 +4,19 @@
  * Slug: jer-twentytwentyfive-child-theme/home
  * Inserter: no
  */
+
+echo '<!-- wp:template-part {"slug":"header"} /-->';
+
+if (!is_paged()) {
+	echo '<!-- wp:template-part {"slug":"jer-intro"} /-->';
+	echo '<!-- wp:template-part {"slug":"jer-widget-grid"} /-->';
+}
+
 ?>
-
-
-<!-- wp:template-part {"slug":"header"} /-->
-
-<!-- wp:template-part {"slug":"jer-intro"} /-->
-
-<!-- wp:template-part {"slug":"jer-widget-grid"} /-->
-
 <!-- wp:group {"tagName":"main","style":{"spacing":{"margin":{"top":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
 <main class="wp-block-group" style="margin-top:var(--wp--preset--spacing--60)">
 	<!-- wp:heading {"textAlign":"left","level":1,"layout":{"type":"default"}} -->
-	<h1 class="wp-block-heading wp-block-query-title has-text-align-left">Recent blog posts</h1>
+	<h1 class="wp-block-heading wp-block-query-title has-text-align-left">Latest blog posts</h1>
 	<!-- /wp:heading -->
 	<!-- wp:pattern {"slug":"jer-twentytwentyfive-child-theme/jer-template-query-loop-list"} /-->
 </main>
