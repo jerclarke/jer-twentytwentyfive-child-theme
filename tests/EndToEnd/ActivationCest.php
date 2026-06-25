04e96928192772f9300b05fbad5b91e568327aa9
Jer Clarke <jer@simianuprising.com>
2026-06-25 13:39:11 -0600

    ✅ update phpcs config to ignore more formatting issues in test files and fix some auto-fix errors in test files

--- tests/EndToEnd/ActivationCest.php
+++ tests/EndToEnd/ActivationCest.php
@@ -4,13 +4,12 @@ namespace Tests\EndToEnd;
 
 use Tests\Support\EndToEndTester;
 
-class ActivationCest
-{
-    public function test_it_activates_correctly(EndToEndTester $I): void
-    {
-        $I->loginAsAdmin();
-        $I->amOnAdminPage('/themes.php');
+class ActivationCest {
 
-        $I->seeElement('.theme.active[data-slug="jer-twentytwentyfive-child-theme"]');
-    }
+	public function test_it_activates_correctly( EndToEndTester $I ): void {
+		$I->loginAsAdmin();
+		$I->amOnAdminPage( '/themes.php' );
+
+		$I->seeElement( '.theme.active[data-slug="jer-twentytwentyfive-child-theme"]' );
+	}
 }
